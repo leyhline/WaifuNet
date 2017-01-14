094cc5f63ea5b2a6309e994f4efab4dccb51d1cc
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2017-01-14 21:13:10 +0200

    Removed env line for files in src because they are no scripts.

--- src/model.py
+++ src/model.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 This file contains some simple model(s) build for this specific dataset.
--- src/pcloud.py
+++ src/pcloud.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 A simple interface for getting files from pCloud.
