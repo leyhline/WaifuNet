28f15bff4faa79064d68e8a267a3e59daacf2196
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2017-01-17 20:47:22 +0200

    Added method to continue training.

--- train.py
+++ train.py
@@ -17,6 +17,7 @@ Created on Thu Jan 12 21:52:15 2017
 from src.model import WaifuVGG16
 from src.trainingset import TrainingSet
 from keras.callbacks import ModelCheckpoint, CSVLogger
+from keras.optimizers import SGD
 import sys
 import logging
 import logging.config
@@ -25,8 +26,9 @@ import yaml
 
 TRAINING_SAMPLES   = 300000
 VALIDATION_SAMPLES = 100000
-EPOCHES = 1
-BATCH_DIVIDER = 5  # Hard to explain... If this one is bigger 
+EPOCHES = 3
+INITIAL_EPOCH = 0
+BATCH_DIVIDER = 4  # Hard to explain... If this one is bigger 
                    # the batch size will become smaller.
 QUERY_SIZE = 10 * BATCH_DIVIDER
 VERBOSE = 1
@@ -43,21 +45,39 @@ def train():
                     "deeplearning/validation", "deeplearning/validation_txt",
                     batch_divider=BATCH_DIVIDER)
     model = WaifuVGG16()
-    model.compile("sgd",
+    sgd = SGD(lr=0.001)
+    model.compile(sgd,
                   "categorical_crossentropy",
-                  metrics=["categorical_crossentropy"])
-    print("Starting training.")
-    history = model.fit_generator(
-                        tset.training,
-                        TRAINING_SAMPLES,
-                        EPOCHES,
-                        verbose=VERBOSE,
-                        callbacks=[ModelCheckpoint("train.hdf5",
-                                                   save_weights_only=True), 
-                                   CSVLogger("logs/train.log", append=False)],
-                        validation_data=tset.validation,
-                        nb_val_samples=VALIDATION_SAMPLES,
-                        max_q_size=QUERY_SIZE)
+                  metrics=["accuracy", "categorical_accuracy"])
+    if INITIAL_EPOCH:
+        print("Loading model weights: train.hdf5")
+        model.load_weights("train.hdf5")
+        print("Resume training.")
+        history = model.fit_generator(
+                                    tset.training,
+                                    TRAINING_SAMPLES,
+                                    EPOCHES,
+                                    verbose=VERBOSE,
+                                    callbacks=[ModelCheckpoint("train.hdf5",
+                                                               save_weights_only=True), 
+                                               CSVLogger("logs/train.log", append=True)],
+                                    validation_data=tset.validation,
+                                    nb_val_samples=VALIDATION_SAMPLES,
+                                    max_q_size=QUERY_SIZE,
+                                    initial_epoch=INITIAL_EPOCH)
+    else:
+        print("Starting training.")
+        history = model.fit_generator(
+                            tset.training,
+                            TRAINING_SAMPLES,
+                            EPOCHES,
+                            verbose=VERBOSE,
+                            callbacks=[ModelCheckpoint("train.hdf5",
+                                                       save_weights_only=True), 
+                                       CSVLogger("logs/train.log", append=True)],
+                            validation_data=tset.validation,
+                            nb_val_samples=VALIDATION_SAMPLES,
+                            max_q_size=QUERY_SIZE)
     return model, history
 
 
