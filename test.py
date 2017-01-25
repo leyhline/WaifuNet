c200c1eabda721b084a91d2a34ca7a785d137810
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2017-01-25 21:40:53 +0200

    Script for running model over testset.

--- test.py
+++ test.py
@@ -1,42 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Load existing model with trained weights and evaluate
+testset data.
+
+Created on Wed Jan 25 21:05:08 2017
+
 @copyright: 2017 Thomas Leyh
 @licence: GPLv3
 """
 
+from src.model import SimpleConvNet
 from src.trainingset import TrainingSet
-import sys
-import logging
-import logging.config
-import yaml
+from keras.optimizers import SGD
 
 
-TRAINING_SAMPLES   = 300000
-VALIDATION_SAMPLES = 100000
-EPOCHES = 1
-BATCH_DIVIDER = 5  # Hard to explain... If this one is bigger 
+TEST_SAMPLES   = 100000
+BATCH_DIVIDER = 1  # Hard to explain... If this one is bigger 
                    # the batch size will become smaller.
 QUERY_SIZE = 10 * BATCH_DIVIDER
-VERBOSE = 1
-
-# Load and configure logging.
-with open("logging.yaml") as f:
-    logging_config = yaml.load(f)
-logging.config.dictConfig(logging_config)
 
 
 def test():
-    tset = TrainingSet()
-    tset.initialize("deeplearning/training", "deeplearning/training_txt",
-                    "deeplearning/validation", "deeplearning/validation_txt",
+    testset = TrainingSet()
+    testset.initialize("deeplearning/testset", "deeplearning/testset_txt",
                     batch_divider=BATCH_DIVIDER)
-    i = 0
-    while True:
-        x, y = next(tset.training)
-        print(i, x.shape, y.shape)
-        i += 100 // BATCH_DIVIDER
-
-
+    model = SimpleConvNet()
+    sgd = SGD()
+    model.compile(sgd,
+                  "categorical_crossentropy",
+                  metrics=["accuracy"])
+    model.load_weights("train.hdf5")
+    model.evaluate_generator(testset.training,
+                             val_samples=TEST_SAMPLES,
+                             max_q_size=QUERY_SIZE)
 if __name__ == "__main__":
     test()
