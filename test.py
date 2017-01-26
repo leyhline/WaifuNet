57fce75244f036dda4f1f56921654102134063be
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2017-01-26 21:32:58 +0200

    Better test script with precise output

--- test.py
+++ test.py
@@ -10,15 +10,19 @@ Created on Wed Jan 25 21:05:08 2017
 @licence: GPLv3
 """
 
+
+import numpy as np
 from src.model import SimpleConvNet
 from src.trainingset import TrainingSet
-from keras.optimizers import SGD
 
 
 TEST_SAMPLES   = 100000
-BATCH_DIVIDER = 1  # Hard to explain... If this one is bigger 
+BATCH_DIVIDER = 4  # Hard to explain... If this one is bigger 
                    # the batch size will become smaller.
 QUERY_SIZE = 10 * BATCH_DIVIDER
+MAPPING = np.array(("Dress", "Japanese Clothes", "Nude",
+                    "School Uniform", "Shirt", "Swimsuit"),
+                    dtype=np.unicode)
 
 
 def test():
@@ -26,14 +30,31 @@ def test():
     testset.initialize("deeplearning/testset", "deeplearning/testset_txt",
                     batch_divider=BATCH_DIVIDER)
     model = SimpleConvNet()
-    sgd = SGD()
-    model.compile(sgd,
-                  "categorical_crossentropy",
-                  metrics=["accuracy"])
     model.load_weights("train.hdf5")
-    return model.evaluate_generator(testset.training,
-                                    val_samples=TEST_SAMPLES,
-                                    max_q_size=QUERY_SIZE)
+    correct = np.zeros(6, dtype=np.int32)
+    incorrect = np.zeros((6, 6), dtype=np.int32)
+    i = 0
+    print(i, TEST_SAMPLES, sep="/")
+    while i < TEST_SAMPLES:
+        img, val = next(testset.training)
+        preds = model.predict_classes(img)
+        val = map(np.argmax, val)
+        assert len(preds) == len(val)
+        for i in range(len(preds)):
+            if preds[i] == val[i]:
+                correct[preds[i]] += 1
+            else:
+                incorrect[preds[i], val[i]] += 1
+        i += len(preds)
+        print(i, TEST_SAMPLES, sep="/")
+    return correct, incorrect
+
+
 if __name__ == "__main__":
     # TODO What is the output? Where to get it from?
-    test()
+    correct, incorrect = test()
+    print(MAPPING)
+    print("Correctly predicted:")
+    print(correct)
+    print("Incorrectly predicted:")
+    print(incorrect)
